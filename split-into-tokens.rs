2974b41bad98fd02bf2f5b4e2ea3a1f3c4243fd2
Konstantine Kharlamov <Hi-Angel@yandex.ru>
2018-05-20 21:35:16 +0300

    Take tokens frequency into account
    Signed-off-by: Konstantine Kharlamov <Hi-Angel@yandex.ru>

--- split-into-tokens.rs
+++ split-into-tokens.rs
@@ -1,11 +1,12 @@
 // 1. to hound the app into source code files ✓
-// 2. buld an array of tokens
+// 2. buld an array of tokens ✓
 // 3. collapse {tokens} → {tokens, frequency}, where each token appears once.
 // ... profit! Actually, let's look at the results of 3-rd step before proceeding.
 
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::BTreeMap;
 
 #[derive(Debug)]
 enum TokenTypes {
@@ -13,11 +14,15 @@ enum TokenTypes {
     Uninitialized,
     AlphaNumeric, // alphanumeric, _
     Specials // non-alphanumeric, but specials, _, spaces, newlines.
+        //todo: add "Finished" to move last processee from processee
 }
+
+type Freq = u32;
 
 #[derive(Debug)]
 struct TokenizeState {
-    vec: Vec<String>,
+    tokens: BTreeMap<String, Freq>,
+    processee: String,
     type_processed: TokenTypes
 }
 
@@ -25,24 +30,25 @@ fn tokenize(mut state: TokenizeState, ch: char) -> TokenizeState {
     match state.type_processed {
         TokenTypes::AlphaNumeric => {
             if ch.is_alphanumeric() || ch == '_' {
-                state.vec.last_mut().unwrap().push(ch);
+                state.processee.push(ch);
                 return state;
-            } else {
-                state.vec.push(String::new()); // start a new token
+            } else { // finish a token, start a new one
+                *state.tokens.entry(state.processee.clone()).or_insert(0) += 1;
+                state.processee = String::new();
                 return tokenize(TokenizeState{type_processed: TokenTypes::Specials, ..state}, ch);
             }
         }
         TokenTypes::Specials => {
             if !ch.is_alphanumeric() {
-                state.vec.last_mut().unwrap().push(ch);
+                state.processee.push(ch);
                 return state;
-            } else {
-                state.vec.push(String::new()); // start a new token
+            } else { // finish a token, start a new one
+                *state.tokens.entry(state.processee.clone()).or_insert(0) += 1;
+                state.processee = String::new();
                 return tokenize(TokenizeState{type_processed: TokenTypes::AlphaNumeric, ..state}, ch);
             }
         }
         TokenTypes::Uninitialized => {
-            state.vec.push(String::new()); // start a new token
             if ch.is_alphanumeric() {
                 return tokenize(TokenizeState{type_processed: TokenTypes::AlphaNumeric, ..state}, ch);
             } else {
@@ -55,14 +61,14 @@ fn tokenize(mut state: TokenizeState, ch: char) -> TokenizeState {
 fn main() {
     println!("Please, enter filenames to read form");
     let mut input_data = String::new();
-    let mut state = TokenizeState{vec: Vec::new(), type_processed: TokenTypes::Uninitialized};
+    let mut state = TokenizeState{tokens: BTreeMap::new(), processee: String::new(), type_processed: TokenTypes::Uninitialized};
     for filename in env::args().skip(1) {
         println!("Opening a file {:?}", filename);
         let mut file = File::open(filename).expect("file error");
         file.read_to_string(&mut input_data).expect("Error reading a file");
         state = input_data.chars().fold(state, tokenize);
     }
-    for token in state.vec {
-        println!("{}", token);
+    for (key, value) in state.tokens.iter() {
+        println!("{}: {}", key, value);
     }
 }
